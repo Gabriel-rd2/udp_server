ef7ff2071d18d16a604afb32e85b0e5c5e1c273a
Gabriel-rd2 <gabriel@rd2.ventures>
2023-11-08 21:18:45 -0300

    update

--- udp_server.js
+++ udp_server.js
@@ -1,40 +1,60 @@
-const cv = require("@u4/opencv4nodejs");
-// const cv = require("opencv4nodejs");
+// ===============================================
+// Código de servidor UDP que recebe mídia, e retorna imagem com reconhecimento facial para cliente.
+// Disciplina: Redes de Computadores II - Prof. Elias P. Duarte Jr.
+// Aluna: Gabriel de Almeida Sales Evaristo - GRR20165266
+// ===============================================
 
-const dgram = require("node:dgram");
-const log = console.log;
+// Importação de bibliotecas
+const cv = require("@u4/opencv4nodejs"); // manipulação de imagens
+const ip = require("ip"); // manipulação de endereços IP
+const dgram = require("node:dgram"); // comunicação via UDP
+const { Buffer } = require("node:buffer");
 
-const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2);
-
+// Definição da configuração do servidor
 const conf = {
+  address: ip.address(),
   port: 41234,
 };
 
+// Array para manter o endereço IP dos clientes atendidos
+let clients = [];
+
+// Criação de um classificador para reconhecimento facial
+const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2);
+
+// Função que define o servidor UDP
 const start_server = () => {
+  console.log("Instanciando o Socket ...");
   const server = dgram.createSocket("udp4");
 
+  console.log('Registrando callback para o evento "listening" ...');
   server.on("listening", () => {
     const address = server.address();
-    const port = address.port;
-    const family = address.family;
-    const ipaddr = address.address;
-
-    log("udp_server", "info", "Server is listening at port " + port);
-    log("udp_server", "info", "Server ip :" + ipaddr);
-    log("udp_server", "info", "Server is IP4/IP6 : " + family);
+    console.log(
+      "%s%s",
+      `Socket iniciado! ${address.family} ${conf.address}:${address.port}\n`,
+      "-----------------------------------------------------------------------------\n"
+    );
   });
 
+  console.log('Registrando callback para o evento "message" ...');
   server.on("message", (msg, info) => {
-    log(
-      "udp_server",
-      "info",
-      `Received ${msg.length} bytes from ${info.address}:${info.port}`
+    // Converte a mensagem recebida em um objeto JSON
+    const message = JSON.parse(msg.toString());
+
+    console.log(
+      `A mensagem ${message.order}, com ${msg.length} bytes, foi recebida de ${info.address}:${info.port}!`
     );
 
-    const message = JSON.parse(msg.toString());
+    // Adiciona endereço do remetente em clients,
+    // se já não está lá
+    if (!clients.includes(info.address)) clients.push(info.address);
+
+    // Converte a imagem recebida para um objeto OpenCV
     let buf = Buffer.from(message.image, "base64");
     let image = cv.imdecode(buf);
 
+    // Detecta rostos na imagem e desenha retângulos ao redor deles
     const faceRects = classifier.detectMultiScale(image).objects;
     if (faceRects.length) {
       for (let faceRect of faceRects) {
@@ -42,32 +62,69 @@ const start_server = () => {
       }
     }
 
+    // Prepara os dados para envio de volta ao cliente
     const data = JSON.stringify({
       order: message.order,
       image: cv.imencode(".jpg", image).toString("base64"),
     });
 
-    //sending msg
+    // Envia a mensagem de volta para o cliente
     server.send(data, info.port, info.address, (error, bytes) => {
       if (error) {
-        log("udp_server", "error", error);
+        console.log("Falha ao enviar a mensagem %d!", message.order);
+        console.log("Erro:", error);
+        console.log(
+          "-----------------------------------------------------------------------------"
+        );
+        console.log("Fechando o socket...");
+
+        // No caso de um erro isso é indicado no log e o socket é fechado
         server.close();
       } else {
-        log("udp_server", "info", "Data sent !!!");
+        console.log(
+          `A mensagem ${message.order}, de ${bytes} bytes, foi enviada para ${info.address}:${info.port}!`
+        );
       }
     });
   });
 
+  console.log('Registrando callback para o evento "error" ...');
   server.on("error", (error) => {
-    log("udp_server", "error", error);
+    console.log("Aconteceu um erro durante a execução do programa!");
+    console.log("Erro:", error);
+    console.log(
+      "-----------------------------------------------------------------------------"
+    );
+    console.log("Fechando o socket...");
+
     server.close();
   });
 
+  console.log('Registrando callback para o evento "close" ...');
   server.on("close", () => {
-    log("udp_server", "info", "Socket is closed !");
+    console.log("Socket fechado!\n");
+    console.log(
+      "%s%s%s%s%s",
+      "=============================================================================\n",
+      "Fim da execucao: udp_server.js\n",
+      `Clientes atendidos: ${clients.length}\n`,
+      "=============================================================================\n"
+    );
+    process.exit(0);
   });
 
+  // Faz o bind do servidor na porta especificada
   server.bind(conf.port);
 };
+
+// Inicialização do servidor UDP
+console.log(
+  "%s%s%s%s%s",
+  "=============================================================================\n",
+  "Inicio da execucao: udp_server.js\n",
+  "Disciplina Redes de Computadores II  -  Prof. Elias P. Duarte Jr.\n",
+  "Aluna: Gabriel de Almeida Sales Evaristo - GRR20165266\n",
+  "=============================================================================\n"
+);
 
 start_server();
